e5afbe29349e69ec6df62252c7d66a594b5dc759
icysamon <icysamon@gmail.com>
2024-08-10 17:47:11 +0900

    feat: mouse enter / leave; transparent effect

--- assets/js/border.js
+++ assets/js/border.js
@@ -1,13 +1,27 @@
-const border = document.getElementById( 'border' );
-
-border.addEventListener (
-    'mouseenter',
-    (event) => {
-        event.target.style.color = 'purple';
-    }
-)
-
 var border0 = document.getElementById( 'border' ).content.cloneNode(true);
 document.getElementById( 'container' ).appendChild(border0);
 var border1 = document.getElementById( 'border' ).content.cloneNode(true);
 document.getElementById( 'container' ).appendChild(border1);
+
+document.addEventListener(
+    'DOMContentLoaded',
+    () => {
+        // DOMContentLoaded
+        const widget = document.getElementsByClassName( 'widget' );
+        for (let i = 0; i < widget.length; i++) {
+            // set widget's eventListener
+            widget[i].addEventListener(
+                'mouseenter',
+                (event) => {
+                    widget[i].style.backgroundColor = 'white';
+                }
+            );
+            widget[i].addEventListener(
+                'mouseleave',
+                (event) => {
+                    widget[i].style.backgroundColor = '#f0f8ff';
+                }
+            );
+        }
+    }  
+);
