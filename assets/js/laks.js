1eaab49f62bd93ccc5bf0f06b09de6b237325853
icysamon <icysamon@gmail.com>
2024-08-13 23:29:02 +0900

    refactor: js, css

---
+++ assets/js/laks.js
@@ -0,0 +1,86 @@
+var script = document.createElement('script');
+script.src = "https://cdn.jsdelivr.net/npm/bubbly-bg@1.0.0/dist/bubbly-bg.js";
+
+
+
+script.onload = function() {
+    bubbly({
+        colorStart: '#fff4e6',
+        colorStop: '#ffe9e4',
+        blur: 1,
+        compose: 'source-over',
+        bubbleFunc:() => `hsla(${Math.random() * 50}, 100%, 50%, .3)`
+        });
+    bubbly();
+};
+
+document.head.appendChild(script);
+
+document.addEventListener(
+    'DOMContentLoaded',
+    () => {
+        // DOMContentLoaded
+        const widget = document.getElementsByClassName( 'widget' );
+        for (let i = 0; i < widget.length; i++) {
+            // set widget's eventListener
+            widget[i].addEventListener(
+                'mouseenter',
+                (event) => {
+                    widget[i].style.backgroundColor = 'white';
+                }
+            );
+            widget[i].addEventListener(
+                'mouseleave',
+                (event) => {
+                    widget[i].style.backgroundColor = 'rgba(240, 248, 255, 0.5)';
+                }
+            );
+        }
+    }  
+);
+
+document.addEventListener(
+    'DOMContentLoaded',
+    () => {
+        // DOMContentLoaded
+        const container = document.getElementsByClassName( 'container' );
+        for (let i = 0; i < container.length; i++) {
+            // set post's eventListener
+            container[i].addEventListener(
+                'mouseenter',
+                (event) => {
+                    container[i].style.backgroundColor = 'white';
+                }
+            );
+            container[i].addEventListener(
+                'mouseleave',
+                (event) => {
+                    container[i].style.backgroundColor = 'rgba(240, 248, 255, 0.5)';
+                }
+            );
+        }
+    }  
+);
+
+document.addEventListener(
+    'DOMContentLoaded',
+    () => {
+        // DOMContentLoaded
+        const cat = document.getElementsByClassName( 'cat' );
+        for (let i = 0; i < cat.length; i++) {
+            // set post's eventListener
+            cat[i].addEventListener(
+                'mouseenter',
+                (event) => {
+                    cat[i].style.backgroundColor = 'white';
+                }
+            );
+            cat[i].addEventListener(
+                'mouseleave',
+                (event) => {
+                    cat[i].style.backgroundColor = 'rgba(240, 248, 255, 0.5)';
+                }
+            );
+        }
+    }  
+);
--- functions.php
+++ functions.php
@@ -1,6 +1,12 @@
 <?php
-function add_font_awesome() {
-    wp_enqueue_style( 'font-awesome', get_stylesheet_directory_uri().'/assets/css/font-awesome.css');
-    wp_enqueue_style( 'font-awesome.min', get_stylesheet_directory_uri().'/assets/css/font-awesome.min.css');
+function add_css() {
+    wp_enqueue_style( 'laks', get_stylesheet_directory_uri().'/assets/css/laks.css'); // laks style
+    wp_enqueue_style( 'font-awesome.min', get_stylesheet_directory_uri().'/assets/css/font-awesome.min.css'); // font awesome
 }
-add_action('wp_enqueue_scripts', 'add_font_awesome');
+
+function add_script() {
+    wp_enqueue_script( 'laks', get_template_directory_uri().'/assets/js/laks.js', array(), false, true );
+}
+
+add_action('wp_enqueue_scripts', 'add_css');
+add_action('wp_enqueue_scripts', 'add_script');
